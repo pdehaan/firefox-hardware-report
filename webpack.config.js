925639a24adf4ad55f2484591e3dcaa9d187885b
John Karahalis <john.karahalis@gmail.com>
2017-05-19 13:10:08 -0400

    Use two-space indents consistently in source files
    While working on #42, I noticed that some of the spacing in index.html
    was inconsistent, making it hard for me to modify the source without
    getting confused. This commit introduces a consistent two-space indent
    stipulated by .editorconfig.

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/main.js',
-    output: {
-        path: path.join(__dirname, 'build', 'js'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-        ],
-    },
+  entry: './js/main.js',
+  output: {
+    path: path.join(__dirname, 'build', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ],
+  },
 };
